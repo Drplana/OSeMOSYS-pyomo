1d0c1b2ab1e7e41ef6404992c94813e7cdba122d
Sylvain Quoilin <squoilin@ulg.ac.be>
2023-12-30 21:29:19 +0100

    defining a package and correcting path so that it works also with python (not only ipython)

---
+++ OSeMOSYS/__init__.py
@@ -0,0 +1,2 @@
+from .SolveSolutions import solve_model
+from .postprocessing.plots import plot_activity
--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,22 +1,26 @@
 '''
 This file illustrates the use of the osemodsys-pyomo library to run a test case
 
-It load the main function from the library and provides as parameters the input excel file and the folder where
+It loads the main function from the library and provides as parameters the input excel file and the folder where
 the results should be written
 '''
+import os,sys
+# include the main library path (the parent folder) in the path environment variable
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-import os
+#Two solution to import the library
+# as a package (defined in __init__.py) => function calls are
+import OSeMOSYS as om
+
+# as separate functions:
 from OSeMOSYS.SolveSolutions import solve_model
 from OSeMOSYS.postprocessing.plots import plot_activity
 
-# go up one folder to be in the main repository folder
-os.chdir('..')
-
 # Define the input file and the folder to store the results and run the model
-results_folder = 'results'
-input_file = 'data/OsemosysNew.xlsx'
-solve_model(input_file, results_folder)
+results_folder = '../results'
+input_file = '../data/OsemosysNew.xlsx'
+om.solve_model(input_file, results_folder)
 
 # plot the results
 datafile = results_folder + '/v_RateOfActivity.csv'
-plot_activity(datafile)
+om.plot_activity(datafile)
